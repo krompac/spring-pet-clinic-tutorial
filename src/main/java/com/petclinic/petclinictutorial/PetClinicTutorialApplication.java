d98bc66c5894d148aa8088536138325fb8b717c3
Tomislav Podunajec <t.podunaj@gmail.com>
2019-07-24 16:28:24 +0200

    Initialized project.

---
+++ src/main/java/com/petclinic/petclinictutorial/PetClinicTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.petclinictutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petclinic/petclinictutorial/PetClinicTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.petclinic.petclinictutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinicTutorialApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
