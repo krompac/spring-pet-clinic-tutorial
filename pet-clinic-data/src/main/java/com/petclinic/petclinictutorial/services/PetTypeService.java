bcbcae1902f3ccbb814a8000ea5225c25961b248
Tomislav Podunajec <t.podunaj@gmail.com>
2019-07-28 23:11:00 +0200

    Implementiran PetTypeMapService.

---
+++ pet-clinic-data/src/main/java/com/petclinic/petclinictutorial/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.petclinic.petclinictutorial.services;
+
+import com.petclinic.petclinictutorial.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/petclinic/petclinictutorial/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.petclinic.petclinictutorial.services.map;
+
+import com.petclinic.petclinictutorial.model.PetType;
+import com.petclinic.petclinictutorial.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
