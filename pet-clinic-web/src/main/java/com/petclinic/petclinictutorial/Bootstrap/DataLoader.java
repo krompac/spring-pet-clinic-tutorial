95b6a2e56eaa32365174f8e6bc3b69c891dc09d9
Tomislav Podunajec <t.podunaj@gmail.com>
2019-07-28 23:41:13 +0200

    Dodani pettypeovi u bootstrap data loader.

--- pet-clinic-web/src/main/java/com/petclinic/petclinictutorial/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/petclinic/petclinictutorial/Bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package com.petclinic.petclinictutorial.Bootstrap;
 
 import com.petclinic.petclinictutorial.model.Owner;
+import com.petclinic.petclinictutorial.model.PetType;
 import com.petclinic.petclinictutorial.model.Vet;
 import com.petclinic.petclinictutorial.services.OwnerService;
+import com.petclinic.petclinictutorial.services.PetTypeService;
 import com.petclinic.petclinictutorial.services.VetService;
-import com.petclinic.petclinictutorial.services.map.OwnerServiceMap;
-import com.petclinic.petclinictutorial.services.map.VetServiceMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,15 +14,23 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Pero");
         owner1.setLastName("Peric");
