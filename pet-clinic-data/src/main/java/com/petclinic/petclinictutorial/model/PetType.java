5c449c3c5c7e93376a8ce8a6e55624d8e3fb485d
Tomislav Podunajec <t.podunaj@gmail.com>
2019-07-28 22:59:01 +0200

    Implementiran Visit model i azurirani owner i pettype modeli.

--- pet-clinic-data/src/main/java/com/petclinic/petclinictutorial/model/PetType.java
+++ pet-clinic-data/src/main/java/com/petclinic/petclinictutorial/model/PetType.java
@@ -2,4 +2,16 @@ package com.petclinic.petclinictutorial.model;
 
 public class PetType extends BaseEntity{
     private String name;
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
