0a52c9706561d6200410a1c41e294bc72a119bd9
Tomislav Podunajec <t.podunaj@gmail.com>
2019-07-29 00:12:18 +0200

    Implementiran service map za specijalnosti veterinara.

---
+++ pet-clinic-data/src/main/java/com/petclinic/petclinictutorial/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.petclinic.petclinictutorial.services;
+
+import com.petclinic.petclinictutorial.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
